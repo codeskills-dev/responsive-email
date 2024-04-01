e068c36d36f51ea331754063bbd74d0cd0de4928
gabriel miranda <gabrielmfern@outlook.com>
2024-04-01 13:43:56 -0300

    feat: use the new `core` workspace dependency to create the components

--- packages/responsive-react-email/tsup.config.ts
+++ packages/responsive-react-email/tsup.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   format: ["cjs", "esm"],
   entry: ["src/index.ts"],
+  external: ["@react-email/section"],
+  noExternal: ["responsive-email-core"],
   dts: true,
   clean: true,
   splitting: true,
-  sourcemap: true,
   minify: true,
+  sourcemap: true,
 });
